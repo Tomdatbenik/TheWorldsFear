7e1c1b8d6760ae9bd44a485cd4b0936cf0afb35c
Tomdatbenik <tomvankaathoven@gmail.com>
2019-09-20 16:51:30 +0200

    Enemie hits hero

---
+++ The World's Fear/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float Movementspeed = 10f;
+
+    public Rigidbody2D rb;
+
+    private Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.transform.Translate(movement * Movementspeed * Time.fixedDeltaTime);
+    }
+
+}
