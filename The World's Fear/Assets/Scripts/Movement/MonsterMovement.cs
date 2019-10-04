71be1a708d1207db4576de7bcec51f7e4774a187
Tomdatbenik <tomvankaathoven@gmail.com>
2019-10-04 15:04:26 +0200

    Spider walks towards player
    Spider doesn't render

--- The World's Fear/Assets/Scripts/Movement/MonsterMovement.cs
+++ The World's Fear/Assets/Scripts/Movement/MonsterMovement.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class MonsterMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform Target;
+    public Rigidbody2D rb;
+    public float movementspeed;
+
+    private Vector2 movement;
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        this.transform.LookAt(Target);
+
+        rb.transform.position += transform.forward * movementspeed * Time.fixedDeltaTime;
     }
 }
