9e269ed173c9eace58f147ddee1f7b2638052af8
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2019-10-10 12:04:13 +0200

    Attack animation

---
+++ The World's Fear/Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    Vector2 direction;
+    float x;
+    float y;
+    public Animator animator;
+    bool attack = false;
+    int cooldown = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attack == false)
+        {
+            direction.x = Input.GetAxisRaw("Horizontal");
+            direction.y = Input.GetAxisRaw("Vertical");
+        }
+        x = direction.x;
+        y = direction.y;
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            attack = true;
+        }
+        animator.SetBool("Attack", attack);
+        animator.SetFloat("Attack Horizontal", x);
+        animator.SetFloat("Attack Vertical", y);
+        Debug.Log(new Vector2(x,y));
+    }
+
+    private void FixedUpdate()
+    {
+        if(attack == true)
+        {
+            cooldown--;
+            if (cooldown == 0)
+            {
+                attack = false;
+                cooldown = 20;
+            }
+        }
+    }
+    
+}
--- The World's Fear/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ The World's Fear/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -9,14 +9,28 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     public Animator animator;
+    private bool move = true;
 
     private Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxis("Horizontal");
-        movement.y = Input.GetAxis("Vertical");
+        if (move)
+        {
+            Movementspeed = 5;
+            movement.x = Input.GetAxis("Horizontal");
+            movement.y = Input.GetAxis("Vertical");
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            move = false;
+            Movementspeed = 2.5f;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            move = true;
+        }
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
