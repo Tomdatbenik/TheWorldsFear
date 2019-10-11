1fe7cd8cb8abd3533c7f66558d61e7b562de1fd5
wiebj3 <wiebj3@gmail.com>
2019-10-11 10:27:08 +0100

    Abilities reworked

---
+++ The World's Fear/Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability
+{
+    private int Cooldowntime;
+    private int Counter;
+    public bool isCasted = false;
+    public bool canCast = true;
+
+    public Ability(int cooldowntime)
+    {
+        Cooldowntime = cooldowntime;
+    }
+
+    public bool isRecharged()
+    {
+        Counter++;
+        if(Counter >= Cooldowntime && !canCast)
+        {
+            Debug.Log("recharged");
+            isCasted = false;
+            canCast = true;
+            Counter = 0;
+            return true;
+        }
+        return false;
+    }
+
+    public void Cast()
+    {
+        isCasted = true;
+        canCast = false;
+    }
+}
