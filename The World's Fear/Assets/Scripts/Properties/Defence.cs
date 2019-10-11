5caa916ffad3091cc25bfdbb3fe23d8951742c62
Tomdatbenik <tomvankaathoven@gmail.com>
2019-10-11 10:29:39 +0200

    Speed defence health and attack

---
+++ The World's Fear/Assets/Scripts/Properties/Defence.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defence : MonoBehaviour
+{
+    public int DefencePoints;
+    private int InitPoints;
+
+    public Defence()
+    {
+        InitPoints = DefencePoints;
+    }
+
+    public Defence(int DP)
+    {
+        this.DefencePoints = DP;
+        InitPoints = DP;
+    }
+
+    public int GetDefence()
+    {
+        return this.DefencePoints;
+    }
+
+    public void IncreaseDefence(int dp)
+    {
+        this.DefencePoints += dp;
+    }
+
+    public void DecreaseDefence(int dp)
+    {
+        this.DefencePoints -= dp;
+    }
+
+    public void RestoreBackToInit()
+    {
+        DefencePoints = InitPoints;
+    }
+}
