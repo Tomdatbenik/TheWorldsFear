7f41ef63d72f6691a0c91cc5d6ba07629b2af4de
Tomdatbenik <tomvankaathoven@gmail.com>
2019-11-06 21:19:54 +0100

    Sword

---
+++ The World's Fear/Assets/Scripts/Player/Sword.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public GameObject wielder;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector2(wielder.transform.position.x + animator.GetFloat("Horizontal"), wielder.transform.position.y + animator.GetFloat("Vertical"));
+    }
+}
