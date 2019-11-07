f65b14e418288b1914ee0d4b065e4c8eab326a4c
wiebj3 <wiebj3@gmail.com>
2019-11-07 09:53:30 +0100

    started on Riskspotattack

--- The World's Fear/Assets/Scripts/Boss/RiskSpotAttack.cs
+++ The World's Fear/Assets/Scripts/Boss/RiskSpotAttack.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class RiskSpotAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
+    public BoxCollider2D boxCollider;
+    public BoxCollider2D playercollider;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (boxCollider.IsTouching(playercollider))
+        {
+            animator.SetBool("Initiate", true);
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("RiskSpotAttack"))
+        {
+            animator.SetBool("Initiate", false);
+        }
     }
 }
