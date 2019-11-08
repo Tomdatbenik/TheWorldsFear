1c2597681b3a466d30e56d9891a4b8ea5fb94ac8
wiebj3 <wiebj3@gmail.com>
2019-11-08 09:39:36 +0100

    Attack animation fixed

--- The World's Fear/Assets/Scripts/Hero/Attack.cs
+++ The World's Fear/Assets/Scripts/Hero/Attack.cs
@@ -15,18 +15,42 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //get the direction in which the player is moving
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
 
-        //direction.x = Input.GetAxisRaw("Horizontal");
-        //direction.y = Input.GetAxisRaw("Vertical");
+        //detect if the player is attacking or not
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            attack = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            attack = false;
+        }
+        
+        //dectect if the player has last moved into one of the four animation directions
+        if (direction.x != 0 && direction.y == 0)
+        {
+            x = direction.x;
+            y = direction.y;
+        }
+        if(direction.y != 0 && direction.x == 0)
+        {
+            x = direction.x;
+            y = direction.y;
+        }
+        animator.SetBool("Attack", attack);
+        animator.SetFloat("Attack Horizontal", direction.x);
+        animator.SetFloat("Attack Vertical", direction.y);
+        //dectect if the player is moving at all
+        if (direction.x == 0 && direction.y == 0)
+        {
+            animator.SetFloat("Attack Horizontal", x);
+            animator.SetFloat("Attack Vertical", y);
+        }
+        
 
-
-        //if ()
-        //{
-        //    attack = true;
-        //}
-        //animator.SetBool("Attack", attack);
-        //animator.SetFloat("Attack Horizontal", direction.x);
-        //animator.SetFloat("Attack Vertical", direction.y);
     }
 
     private void FixedUpdate()
