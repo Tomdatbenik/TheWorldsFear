97afb5337ae363b12e250996b9fbbcb4a5c3d2ba
Tomdatbenik <tomvankaathoven@gmail.com>
2019-11-13 23:59:28 +0100

    VIllage / forest

--- The World's Fear/Assets/Scripts/Hero/Attack.cs
+++ The World's Fear/Assets/Scripts/Hero/Attack.cs
@@ -4,79 +4,113 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    Vector2 direction;
-    float x;
-    float y;
+    //Vector2 direction;
+    //float x;
+    //float y;
     public Animator animator;
-    bool attack = false;
+    //bool attack = false;
     public GameObject sword;
+    private Speed speed;
 
-    private int timer;
-    public int AttackDuration;
+    //private int timer;
+    //public int AttackDuration;
+
+    public Joystick joystick;
+    public bool CanAttack;
+
+    private bool CanSlow = true;
 
     private void Start()
     {
-        sword.SetActive(false);
+        speed = gameObject.GetComponent(typeof(Speed)) as Speed;
+
+        //sword.SetActive(false);
     }
     // Update is called once per frame
     void Update()
     {
-        //get the direction in which the player is moving
-        direction.x = animator.GetFloat("Horizontal");
-        direction.y = animator.GetFloat("Vertical");
 
-        //detect if the player is attacking or not
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if(joystick.Direction != Vector2.zero)
         {
-            attack = true;
-            timer = 0;
-        }
-
-        if (attack == true)
-        {
-            Debug.Log("Attacking");
+            if(CanSlow)
+            {
+                speed.SetSpeed(speed.GetSpeed() / 2);
+                CanSlow = false;
+            }
+   
+            animator.SetBool("Attack", true);
             sword.SetActive(true);
-            timer++;
         }
-
-        if(timer > AttackDuration)
+        else
         {
-            attack = false;
+            if(!CanSlow)
+            {
+                speed.ResetSpeed();
+            }
+
+            CanSlow = true;
+
+            animator.SetBool("Attack", false);
             sword.SetActive(false);
         }
 
-        SetAnimatorValues();
+
+        ////get the direction in which the player is moving
+        //direction.x = animator.GetFloat("Horizontal");
+        //direction.y = animator.GetFloat("Vertical");
+
+        ////detect if the player is attacking or not
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    attack = true;
+        //    timer = 0;
+        //}
+
+        //if (attack == true)
+        //{
+        //    Debug.Log("Attacking");
+        //    //sword.SetActive(true);
+        //    //timer++;
+        //}
+
+        //if(timer > AttackDuration)
+        //{
+        //    attack = false;
+        //    //sword.SetActive(false);
+        //}
+
+        //SetAnimatorValues();
 
 
     }
 
-    public void CastAttack()
-    {
-        attack = true;
-        timer = 0;
-    }
+    //public void CastAttack()
+    //{
+    //    attack = true;
+    //    timer = 0;
+    //}
 
-    private void SetAnimatorValues()
-    {
-        //dectect if the player has last moved into one of the four animation directions
-        if (direction.x != 0 && direction.y == 0)
-        {
-            x = direction.x;
-            y = direction.y;
-        }
-        if (direction.y != 0 && direction.x == 0)
-        {
-            x = direction.x;
-            y = direction.y;
-        }
-        animator.SetBool("Attack", attack);
-        animator.SetFloat("Attack Horizontal", direction.x);
-        animator.SetFloat("Attack Vertical", direction.y);
-        //detect if the player is moving at all
-        if (direction.x == 0 && direction.y == 0)
-        {
-            animator.SetFloat("Attack Horizontal", x);
-            animator.SetFloat("Attack Vertical", y);
-        }
-    }
+    //private void SetAnimatorValues()
+    //{
+    //    //dectect if the player has last moved into one of the four animation directions
+    //    if (direction.x != 0 && direction.y == 0)
+    //    {
+    //        x = direction.x;
+    //        y = direction.y;
+    //    }
+    //    if (direction.y != 0 && direction.x == 0)
+    //    {
+    //        x = direction.x;
+    //        y = direction.y;
+    //    }
+    //    //animator.SetBool("Attack", attack);
+    //    animator.SetFloat("Attack Horizontal", direction.x);
+    //    animator.SetFloat("Attack Vertical", direction.y);
+    //    //detect if the player is moving at all
+    //    if (direction.x == 0 && direction.y == 0)
+    //    {
+    //        animator.SetFloat("Attack Horizontal", x);
+    //        animator.SetFloat("Attack Vertical", y);
+    //    }
+    //}
 }
--- The World's Fear/Assets/Scripts/Hero/Sword.cs
+++ The World's Fear/Assets/Scripts/Hero/Sword.cs
@@ -7,6 +7,7 @@ public class Sword : MonoBehaviour
     public GameObject wielder;
     public Animator animator;
     public Rigidbody2D rb;
+    public Joystick joystick;
 
     private Vector2 LastRotation;
     // Start is called before the first frame update
@@ -22,50 +23,53 @@ public class Sword : MonoBehaviour
         float angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
 
-        Vector2 AnimatorFloats = new Vector2(animator.GetFloat("Horizontal"), animator.GetFloat("Vertical"));
+        Vector2 DirectionFloats = new Vector2(joystick.Direction.x, joystick.Direction.y);
 
-        if (AnimatorFloats.x > 0)
-        {
-            LastRotation.x = 1;
-            AnimatorFloats.x = 1;
-        }
-        else if(AnimatorFloats.x < 0)
-        {
-            LastRotation.x = -1;
-            AnimatorFloats.x = -1;
-        }
-        else if(AnimatorFloats.x == 0 && AnimatorFloats.y != 0)
-        {
-            LastRotation.x = 0;
-        }
+        //if (DirectionFloats.x > 0)
+        //{
+        //    LastRotation.x = 1;
+        //    DirectionFloats.x = 1;
+        //}
+        //else if(DirectionFloats.x < 0)
+        //{
+        //    LastRotation.x = -1;
+        //    DirectionFloats.x = -1;
+        //}
+        //else if(DirectionFloats.x == 0 && DirectionFloats.y != 0)
+        //{
+        //    LastRotation.x = 0;
+        //}
 
-        if (AnimatorFloats.y > 0)
-        {
-            LastRotation.y = 1;
-            AnimatorFloats.y = 1;
-        }
-        else if (AnimatorFloats.y < 0)
-        {
-            LastRotation.y = -1;
-            AnimatorFloats.y = -1;
-        }
-        else if (AnimatorFloats.y == 0 && AnimatorFloats.x != 0)
-        {
-            LastRotation.y = 0;
-        }
+        //if (DirectionFloats.y > 0)
+        //{
+        //    LastRotation.y = 1;
+        //    DirectionFloats.y = 1;
+        //}
+        //else if (DirectionFloats.y < 0)
+        //{
+        //    LastRotation.y = -1;
+        //    DirectionFloats.y = -1;
+        //}
+        //else if (DirectionFloats.y == 0 && DirectionFloats.x != 0)
+        //{
+        //    LastRotation.y = 0;
+        //}
 
-        Debug.Log("x " + LastRotation.x);
-        Debug.Log("y " + LastRotation.y);
+        //Debug.Log("x " + LastRotation.x);
+        //Debug.Log("y " + LastRotation.y);
 
-        if (AnimatorFloats == Vector2.zero)
-        {
-            //TODO Remember postion player is facing
-            AnimatorFloats.x = LastRotation.x;
-            AnimatorFloats.y = LastRotation.y;
-        }
+        //if (DirectionFloats == Vector2.zero)
+        //{
+        //    //TODO Remember postion player is facing
+        //    DirectionFloats.x = LastRotation.x;
+        //    DirectionFloats.y = LastRotation.y;
+        //}
 
-        Debug.Log(AnimatorFloats.x);
-        Debug.Log(AnimatorFloats.y);
-        this.transform.position = new Vector2(wielder.transform.position.x + AnimatorFloats.x, wielder.transform.position.y + AnimatorFloats.y);
+        //Debug.Log(DirectionFloats.x);
+        //Debug.Log(DirectionFloats.y);
+
+        this.transform.position = new Vector2(wielder.transform.position.x + DirectionFloats.x, wielder.transform.position.y + DirectionFloats.y);
+        animator.SetFloat("Attack Horizontal", DirectionFloats.x);
+        animator.SetFloat("Attack Vertical", DirectionFloats.y);
     }
 }
