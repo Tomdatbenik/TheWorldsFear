6d93dddbd8a3312b07881596324dfaa95c191c40
Tomdatbenik <tomvankaathoven@gmail.com>
2019-11-14 11:44:41 +0100

    Village complete

---
+++ The World's Fear/Assets/Scripts/Map/PickUpSword.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpSword : MonoBehaviour
+{
+    public Joystick joystick;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        joystick.gameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
+
--- The World's Fear/Assets/UI/StartGame.cs
+++ The World's Fear/Assets/UI/StartGame.cs
@@ -8,6 +8,6 @@ public class StartGame : MonoBehaviour
 
     public void GameStart()
     {
-        SceneManager.LoadScene(sceneName: "BossBattle");
+        SceneManager.LoadScene(sceneName: "Village");
     }
 }
