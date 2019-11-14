1559a64580c6e9bc0681ce816bd0cecd0e3d6e45
Tomdatbenik <tomvankaathoven@gmail.com>
2019-11-14 14:47:26 +0100

    Spider sounds

---
+++ The World's Fear/Assets/Scripts/Sound/SpiderSoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderSoundManager : MonoBehaviour
+{
+    public AudioClip DeathSound;
+    public AudioSource audiosource;
+    public Health health;
+
+    private bool DeathSoundPlayed = false;
+
+
+    public void Update()
+    {
+        if(health.isDead())
+        {
+            if(!DeathSoundPlayed)
+            {
+                DeathSoundPlayed = true;
+                audiosource.PlayOneShot(DeathSound);
+            }
+        }
+    }
+}
