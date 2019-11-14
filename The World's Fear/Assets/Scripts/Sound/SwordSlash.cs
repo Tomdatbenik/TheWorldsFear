4900180e6863df34d0477a6f3ea2ac769bd3f0f7
wiebj3 <wiebj3@gmail.com>
2019-11-14 13:54:33 +0100

    sword has slashing sound, added dash sound

---
+++ The World's Fear/Assets/Scripts/Sound/SwordSlash.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSlash : MonoBehaviour
+{
+    public AudioClip[] swordslashes;
+    public AudioSource audiosource;
+
+    int selectswordslash;
+    bool play = false;
+    int counter = 0;
+
+    
+
+    void FixedUpdate()
+    {
+        if(play == false)
+        {
+            play = true;
+            selectswordslash = Random.Range(1, 3);
+            audiosource.clip = swordslashes[selectswordslash];
+            audiosource.Play();
+        }
+        counter++;
+        if (counter == 20)
+        {
+            counter = 0;
+            play = false;
+        }
+    }
+}
